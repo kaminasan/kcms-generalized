81ee0f101bf18ba7b3d4ef258e3ae4dcc8e41d40
KaminaSan <>
2016-02-26 13:18:59 +0900

    Switched URL routing to inside Servlet

--- src/com/kam/cms/controllers/post/PostController.java
+++ src/com/kam/cms/controllers/post/PostController.java
@@ -8,7 +8,6 @@ package com.kam.cms.controllers.post;
 
 import com.kam.SQL.DAO.PostDAO;
 import com.kam.cms.beans.PostBean;
-import com.kam.cms.beans.RecipeBean;
 import com.kam.cms.filecontrol.ImageLister;
 import com.kam.cms.validators.ParamValidator;
 
@@ -16,14 +15,16 @@ import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author Blacksteath
+ * @author KaminaSan <www.kaminasan.com>
  */
+@WebServlet(name="PostController", urlPatterns = {"/post/*"})
 public class PostController extends HttpServlet {
    
    
