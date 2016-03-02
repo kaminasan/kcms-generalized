b93d3290c8ad49c9a0fb124421f504498cc35956
KaminaSan <>
2016-03-02 13:59:18 +0900

    ReRouted Delete Post Controller

--- src/com/kam/cms/controllers/post/DeletePostController.java
+++ src/com/kam/cms/controllers/post/DeletePostController.java
@@ -16,6 +16,7 @@ import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -24,6 +25,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author KaminaSan <www.kaminasan.com>
  */
+@WebServlet(name="DeletePostController", urlPatterns = {"/post/delete/*"})
 public class DeletePostController extends HttpServlet {
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
