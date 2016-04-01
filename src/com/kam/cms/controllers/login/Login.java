eab05cf7f3306555d8c101fd3af2635ab2118c04
KaminaSan <>
2016-04-01 15:43:41 +0900

    added login error messages to the angular app

--- src/com/kam/cms/controllers/login/Login.java
+++ src/com/kam/cms/controllers/login/Login.java
@@ -16,6 +16,7 @@ import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -24,6 +25,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author KaminaSan <www.kaminasan.com>
  */
+@WebServlet(name="LoginController", urlPatterns={"/login"})
 public class Login extends HttpServlet {
    
    
@@ -33,15 +35,19 @@ public class Login extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        RequestDispatcher view = request.getRequestDispatcher("/userlogin");
-        if(request.getSession().getAttribute("user") == null){
-         view.forward(request, response);
-    }
-        else{
-            request.setAttribute("errorMessage", "You shouldn't have done that.");
-            view= request.getRequestDispatcher("/");
-            view.forward(request, response);
+        RequestDispatcher view = request.getRequestDispatcher("/");
+        String backPage = (String) request.getAttribute("backPage");
+        if(backPage == null || backPage.isEmpty() ){
+            backPage = request.getHeader("referer");
         }
+         if( request.getSession().getAttribute("user") == null){
+            request.setAttribute("errorMessage", "You must login or register first! Please click the login button");
+            request.setAttribute("backPage", backPage);
+            view = request.getRequestDispatcher(backPage);
+            view.forward(request, response);
+         }
+           
+        
     } 
 
  
@@ -52,23 +58,25 @@ public class Login extends HttpServlet {
         UserDAO dao = new UserDAO();
         String userPass = request.getParameter("userName");
         String userName = request.getParameter("userPass");
-        String referPage = request.getParameter("backPage");
+        String backPage = (String) request.getAttribute("backPage");
+        if(backPage == null || backPage.isEmpty() ){
+            backPage = request.getHeader("referer");
+        }
         System.out.println("Now attempting to call getSpecificUser with username: " + userName + "and password: " + userPass);
-        //Leaving out salt and hash related files on github for security purposes
         boolean userExists = dao.userInDatabase(userName, userPass);
         System.out.println("UserExists: " +userExists);
-        
         if(!userExists){
-            System.out.println("Attempting to forward user to " + "/userlogin");
-            request.setAttribute("errorMessage", "Error, username or password INCORRECT");
+            view = request.getRequestDispatcher(backPage);
+            request.setAttribute("ErrorMessage", "Error, User does not exist.");
+            request.setAttribute("backPage", backPage);
             view.forward(request, response);
         }
         else{
          
         UserBean returnUser = dao.getSpecificUser(userName,userPass);
         request.getSession().setAttribute("user", returnUser);
-        System.out.println("User successfully logged in. Sending back to: " + request.getContextPath());
-        response.sendRedirect(request.getContextPath());
+        System.out.println("User successfully logged in. Sending back to: " + backPage);
+        response.sendRedirect(backPage);
     }
     }
 
