a3b0e78120b182b23bc5624bbe4f5ed6807afadf
KaminaSan <>
2016-07-14 17:14:10 +0900

    Added Bcrypt for hash/salt

--- src/com/kam/cms/saltnhash/HashGenerator.java
+++ src/com/kam/cms/saltnhash/HashGenerator.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package com.kam.cms.saltnhash;
-
+import org.mindrot.jbcrypt.BCrypt;
 /**
  *
  * @author KaminaSan <www.kaminasan.com>
@@ -12,13 +12,20 @@ package com.kam.cms.saltnhash;
 public class HashGenerator {
     
    private static HashGenerator generatorInstance = new HashGenerator();
-  
+  private static final int LOADLEVEL = 12;
    
-   private static String getHash(String stringToHash){
+   private  String getHash(String stringToHash){
        
+       String hashedString = BCrypt.hashpw(stringToHash, BCrypt.gensalt(LOADLEVEL));
        
-       return null; 
+       return hashedString; 
     }
+   
+   private String testMatch(String hashedPass, String plainTextPass){
+       boolean matched= BCrypt.checkpw(plainTextPass, hashedPass);
+       String result = (matched) ? "Password Matched": "No match";
+       return result;   
+   }
    
    public static HashGenerator getInstance(){
        if(generatorInstance == null ) {
