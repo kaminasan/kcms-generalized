875b46f4788a0c16cf7fbefbd078b8d244e1a2d4
Blacksteath <Blacksteath@KamLaptop>
2016-11-12 18:04:21 +0900

    Used HashGen test to show same object, aka singleton

--- src/com/kam/cms/saltnhash/HashGeneratorTester.java
+++ src/com/kam/cms/saltnhash/HashGeneratorTester.java
@@ -13,6 +13,7 @@ public class HashGeneratorTester {
     public static void main(String[] args) {
        HashGenerator hashGen1 = HashGenerator.getInstance();
        HashGenerator hashGen2 = HashGenerator.getInstance();
+       System.out.println("Hash Gen 1 and 2 are the same object:" + hashGen1.equals(hashGen2));
        System.out.println("Tester Finished");
         
         
